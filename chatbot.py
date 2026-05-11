5454f9233d9325d8fa05a9c504dc015e334d5952
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-12 01:37:48 +0530

    message placeholder

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,22 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage,HumanMessage,AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model='gemini-2.5-flash-lite')
 
+chat_history = [
+    SystemMessage(content='You are a helpful ai assistant')
+]
+
 while True:
     user_input = input('You: ')
+    chat_history.append(HumanMessage(content=user_input))
     if user_input == 'exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(AIMessage(content=result.text))
     print("AI: ", result.text)
+
+print(chat_history)
---
+++ messagePlaceholder.py
@@ -0,0 +1,14 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+#chat template
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful customer support agent'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human', '{query}')
+])
+
+#load chat history
+with open('chat_history.txt') as f:
+    f.readlines
+
+#create prompt
