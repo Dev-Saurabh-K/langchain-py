a223d91ea34c7a9013c3d4333d9b2d6fdafb3305
Dev-Saurabh-K <saurabhkumar.sakr@gmail.com>
2026-05-13 19:28:13 +0530

    pydantic

---
+++ StructuredOutput/pydantic_st.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+
+class Student(BaseModel):
+    name: str = 'nitish'
+    age: Optional[int] = None
+    email: EmailStr
+    cgpa: float = Field(gt=0, lt=10, description='A decimal value representing the cgpa of the student')
+
+# new_student = {'age':32}
+
+# intellient type conversion by pydantic from string to number is number detected
+new_student = {'age':"32", "email":"abc@gmail.com", 'cgpa':5}
+
+
+# inbuilt email validator -pydantic[email]
+
+student = Student(**new_student)
+
+student_dict = dict(student)
+
+print(student_dict['age'])
+
+student_json = student.model_dump_json()
+
+print(student_json)
+
+print(student)
